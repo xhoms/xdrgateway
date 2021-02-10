ec7704ec15cd9139bb662df4ba924630e3e55218
Xavier Homs <xhoms@paloaltonetworks.com>
2021-02-10 12:43:16 +0000

    example section

--- example_test.go
+++ example_test.go
@@ -1,14 +1,16 @@
 package xdrgateway
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 	"time"
 )
 
-// ExampleClient shows how to create an XDRCllient from environmentala variables to send an alert.
+// ExampleXDRClient_fromenv shows how to create an XDRCllient from environmentala variables to send an alert.
 //
 // Notice NewXDRClientFromEnv() will throw a fatal error if the mandatory variables are not found
-func ExampleClient() {
+func ExampleXDRClient_fromenv() {
 	client := NewXDRClientFromEnv()
 	alert := &Alert{
 		LocalIP:          "15.14.13.12",
@@ -26,11 +28,11 @@ func ExampleClient() {
 	}
 }
 
-// ExampleClientExplicit creates a XDRClient explicitly and pushed multiple alerts in a single update
+// ExampleXDRClient_explicit creates a XDRClient explicitly and pushed multiple alerts in a single update
 //
 // Notice the client do not enforce the maximum number of alerts in a single update at it might be
 // rejected at the XDR side as it do not accept more than 60 alerts in a single update
-func ExampleClientExplicit() {
+func ExampleXDRClient_explicit() {
 	client := XDRClient{
 		APIKey:   "O4Bw...wEX",
 		APIKeyID: "37",
@@ -66,4 +68,18 @@ func ExampleClientExplicit() {
 	if err := client.SendMulti(alert); err != nil {
 		log.Fatal(err)
 	}
+}
+
+// ExampleAPI implements a HTTP server that can be used to ingest PAN-OS alerts into Cortex XDR
+// leveraging the NGFW's HTTP Log Forwarding feature
+func ExampleAPI() {
+	port := "8080"
+	client := NewXDRClientFromEnv()
+	parser := NewBasicParser(0)
+	api := NewAPI(parser, client, "", false, nil)
+	http.HandleFunc("/in", api.Ingestion)
+	log.Println("payload template to be used in the PAN-OS device")
+	fmt.Println(string(parser.DumpPayloadLayout()))
+	log.Println("starting http service on port", port)
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
